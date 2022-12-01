6d5f5fcfa567d8d35de7e52c8dca2bfc60c898ed
8-Lambda-8 <j.wasle111@gmail.com>
2022-12-01 17:00:51 +0100

    init

---
+++ index.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+
+let year = "2022";
+let day = "01";
+let part = 0; //0 = both
+
+const dayFolderPath = "./" + year + "/" + day + "/";
+const dayModule = await import(dayFolderPath + "index.js");
+
+const inputText = fs.readFileSync(dayFolderPath + "input", "utf-8");
+
+console.log("###################################################################\n");
+const timeStart = performance.now();
+
+const { part1, part2 } = dayModule.solve(inputText);
+
+if (part !== 2) {
+  console.log("Part1:\n");
+  part1();
+  console.log("\n");
+}
+
+if (part !== 1) {
+  console.log("Part2:\n");
+  part2();
+  console.log("\n");
+}
+
+const timeEnd = performance.now();
+console.log(`\n took ${timeEnd - timeStart}ms`);
---
+++ utils.ts
@@ -0,0 +1,3 @@
+export function solution(solutiuonValue: string | number) {
+  console.log("%c\nSolution: " + solutiuonValue.toString() + "\n","color:red; background-color:blue");
+}
