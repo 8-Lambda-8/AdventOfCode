e61bdd22ba4725a62690c0bfe981e5c02e9ba59b
8-Lambda-8 <j.wasle111@gmail.com>
2022-12-01 18:28:50 +0100

    day 01

---
+++ 2022/01/index.ts
@@ -0,0 +1,26 @@
+import { solution } from "../../utils.js";
+
+export const solve = (inputText: string) => {
+  const elfBlocks = inputText.split("\n\n");
+
+  let elfCalories = new Array<number>();
+  for (const elfBlock of elfBlocks) {
+    let calories = 0;
+    for (const line of elfBlock.split("\n")) {
+      calories += parseInt(line);
+    }
+    elfCalories.push(calories);
+  }
+
+  elfCalories = elfCalories.sort((a, b) => b - a);
+
+  return {
+    part1: () => {
+      solution(Math.max(...elfCalories));
+    },
+
+    part2: () => {
+      solution(elfCalories.splice(0, 3).reduce((part, a) => part + a, 0));
+    },
+  };
+};
