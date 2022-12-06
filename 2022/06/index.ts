8e4f96d41109e1ad5b9927521b8be78f06303cff
8-Lambda-8 <j.wasle111@gmail.com>
2022-12-06 17:31:13 +0100

    solved Day6 part2

--- 2022/06/index.ts
+++ 2022/06/index.ts
@@ -1,24 +1,27 @@
 export const solve = (inputText: string) => {
-  let solution1 = 0;
-  for (let i = 4; i < inputText.length; i++) {
-    let slice = inputText.slice(i - 4, i).split("");
-    let diffCount = new Set(slice).size;
-
-    if (diffCount >= 4) {
-      solution1 = i;
-      break;
-    }
-  }
-
-  const out = "";
-
   return {
     part1: () => {
-      return solution1.toString();
+      let i = 4;
+      for (i; i < inputText.length; i++) {
+        let slice = inputText.slice(i - 4, i).split("");
+        let diffCount = new Set(slice).size;
+
+        if (diffCount >= 4) break;
+      }
+
+      return i.toString();
     },
 
     part2: () => {
-      return out.toString();
+      let i = 14;
+      for (i; i < inputText.length; i++) {
+        let slice = inputText.slice(i - 14, i).split("");
+        let diffCount = new Set(slice).size;
+
+        if (diffCount >= 14) break;
+      }
+
+      return i.toString();
     },
   };
 };
