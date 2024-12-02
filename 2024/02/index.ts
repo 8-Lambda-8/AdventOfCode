a22a23dca04fc1d3b6d84e957fe3c2a356f62e1d
8-Lambda-8 <j.wasle111@gmail.com>
2024-12-02 17:59:09 +0100

    solved day2 part2

--- 2024/02/index.ts
+++ 2024/02/index.ts
@@ -7,30 +7,44 @@ export const solve = (inputText: string) => {
     reports.push(line.split(" ").map((x) => +x));
   }
 
+  function isSave(report: number[]) {
+    let save = true;
+    const increasing = report[0] < report[1];
+
+    for (let i = 0; i < report.length - 1; i++) {
+      if (+report[i] == report[i + 1]) save = false;
+      if (increasing && report[i] > report[i + 1]) save = false;
+      if (!increasing && report[i] < report[i + 1]) save = false;
+      if (Math.abs(+report[i] - +report[i + 1]) > 3) save = false;
+    }
+    return save;
+  }
+
   return {
     part1: () => {
       let saveReports = 0;
       for (const report of reports) {
-        let save = true;
-
-        const increasing = report[0] < report[1];
-
-        for (let i = 0; i < report.length - 1; i++) {
-          if (+report[i] == report[i + 1]) save = false;
-          if (increasing && report[i] > report[i + 1]) save = false;
-          if (!increasing && report[i] < report[i + 1]) save = false;
-          if (Math.abs(+report[i] - +report[i + 1]) > 3) save = false;
-          console.log(report[i], report[i + 1], Math.abs(report[i] - report[i + 1]));
-        }
-        console.log(save);
-        if (save) saveReports++;
+        if (isSave(report)) saveReports++;
       }
 
       return saveReports.toString();
     },
 
     part2: () => {
-      return "out".toString();
+      let saveReports = 0;
+      for (const report of reports) {
+        if (isSave(report)) saveReports++;
+        else {
+          for (let i = 0; i < report.length; i++) {
+            const reportCopy = report.filter((x, j) => j != i);
+            if (isSave(reportCopy)) {
+              saveReports++;
+              break;
+            }
+          }
+        }
+      }
+      return saveReports.toString();
     },
   };
 };
